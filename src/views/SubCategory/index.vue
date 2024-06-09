b33c5624dbe9301b23a07a52e927c06319b42f89
nanapre <3050411966@qq.com>
2024-06-09 21:37:35 +0800

    二级分类-列表筛选

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -25,6 +25,13 @@ const getSubCategoryGoodsData = async () => {
     console.log("SubCategoryGoodsData", SubCategoryGoodsData.value)
 }
 
+const tabChange = () => {
+    console.log('tab切换了', reqData.value.sortField)
+    reqData.value.page = 1
+    getSubCategoryGoodsData()
+
+}
+
 onMounted(() => {
     getSubGategoryData()
     getSubCategoryGoodsData()
@@ -44,7 +51,7 @@ onMounted(() => {
             </el-breadcrumb>
         </div>
         <div class="sub-container">
-            <el-tabs>
+            <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
                 <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
